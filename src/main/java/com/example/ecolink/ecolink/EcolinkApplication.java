de96421af78823f9e1f245dc517e665fd6ef94e2
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-01-22 12:04:22 +0100

    Initialize Spring Boot project structure and configuration files

---
+++ src/main/java/com/example/ecolink/ecolink/EcolinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecolink.ecolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecolink/ecolink/EcolinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecolink.ecolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
