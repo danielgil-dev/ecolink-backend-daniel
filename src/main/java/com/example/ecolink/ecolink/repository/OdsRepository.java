a5c5fde5efcd4e60873f4dee12c7ef03e6c273e0
Lorenzo <ls.krastev@iesdoctorbalmis.com>
2025-01-23 10:13:37 +0100

    Add repository and service classes for Ods, Post, and Proposal; configure application properties for H2 database

--- src/main/java/com/example/ecolink/ecolink/repository/OdsRepository.java
+++ src/main/java/com/example/ecolink/ecolink/repository/OdsRepository.java
@@ -3,9 +3,9 @@ package com.example.ecolink.ecolink.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.ecolink.ecolink.entity.Proposal;
+import com.example.ecolink.ecolink.entity.Ods;
 
 @Repository
-public interface ProposalRepository extends JpaRepository<Proposal, Long> {
-    
+public interface OdsRepository extends JpaRepository<Ods, Long> {
+
 }
---
+++ src/main/java/com/example/ecolink/ecolink/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.ecolink.ecolink.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+	protected BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+    @Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(auth -> auth
+					.requestMatchers("/**").permitAll()
+					.anyRequest().authenticated()
+			)
+			.csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**") // Deshabilitar CSRF para H2 Console
+            )
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.disable()) // Permitir frames para H2 Console
+            )
+			.logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/public")  // Redirige a la página de inicio de sesión después del cierre de sesión
+			.invalidateHttpSession(true)
+            .permitAll()
+        	);
+		return http.build();
+	}
+}
