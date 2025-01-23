458fdad2eb600232dc857a788b0fc007b0e469f9
dagil23 <da.gil@iesdoctorbalmis.com>
2025-01-23 10:36:54 +0100

    Making the services and the repository of the entity mision

---
+++ src/main/java/com/example/ecolink/ecolink/repository/MisionRepository.java
@@ -0,0 +1,9 @@
+package com.example.ecolink.ecolink.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ecolink.ecolink.entity.Mision;
+
+public interface MisionRepository extends JpaRepository<Mision,Long> {
+
+}
---
+++ src/main/java/com/example/ecolink/ecolink/service/MisionService.java
@@ -0,0 +1,12 @@
+package com.example.ecolink.ecolink.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.ecolink.ecolink.repository.MisionRepository;
+
+public class MisionService {
+
+        @Autowired
+        private MisionRepository misionRepository;
+        
+}
