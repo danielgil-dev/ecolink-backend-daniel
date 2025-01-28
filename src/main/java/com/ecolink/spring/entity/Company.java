4fe6202db6148fe642920cec59d7e34b4ddb9ad6
dagil23 <da.gil@iesdoctorbalmis.com>
2025-01-28 11:19:14 +0100

    Fixed errors

--- src/main/java/com/ecolink/spring/entity/Company.java
+++ src/main/java/com/ecolink/spring/entity/Company.java
@@ -1,10 +1,6 @@
 package com.ecolink.spring.entity;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
-import java.time.LocalDate;
->>>>>>> 92f4000ac3cf9a765d6ca912a6f612e4aae61175
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
