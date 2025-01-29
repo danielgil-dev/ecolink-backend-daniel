4296b331b7dd931417b976779396d9cf7466e8f1
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-01-29 09:50:06 +0100

    Added cors

--- src/main/java/com/ecolink/spring/config/MainConfiguration.java
+++ src/main/java/com/ecolink/spring/config/MainConfiguration.java
@@ -3,11 +3,26 @@ package com.ecolink.spring.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MainConfiguration {
     @Bean
     protected ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    protected WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .maxAge(3600);
+            }
+        };
     }
 }
