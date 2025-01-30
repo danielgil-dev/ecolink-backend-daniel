817d969245cf984fafc29963ecec00a99427af8c
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-01-30 09:31:45 +0100

    Finished exception challenge

--- src/main/java/com/ecolink/spring/controller/ChallengeController.java
+++ src/main/java/com/ecolink/spring/controller/ChallengeController.java
@@ -7,6 +7,8 @@ import com.ecolink.spring.dto.ChallengeDTO;
 import com.ecolink.spring.dto.DTOConverter;
 import com.ecolink.spring.entity.Challenge;
 import com.ecolink.spring.entity.Ods;
+import com.ecolink.spring.exception.ChallengeNotFoundException;
+import com.ecolink.spring.exception.ErrorDetails;
 import com.ecolink.spring.service.ChallengeService;
 import com.ecolink.spring.service.OdsService;
 
@@ -16,6 +18,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,39 +30,59 @@ public class ChallengeController {
 
     private final ChallengeService challengeService;
     private final DTOConverter challengeDtoConverter;
-     private final OdsService odsService;
+    private final OdsService odsService;
 
     @GetMapping
     public ResponseEntity<?> getAllChallenges() {
 
-        List<Challenge> challenges = challengeService.getAllChallenges();
-        if (challenges.isEmpty()) {
-            return ResponseEntity.badRequest().build();
+        try {
+            List<Challenge> challenges = challengeService.getAllChallenges();
+            if (challenges.isEmpty()) {
+                throw new ChallengeNotFoundException("No se han encontrado challenges en la base de datos");
+            }
+            List<ChallengeDTO> dtoList = challenges.stream().map(challengeDtoConverter::converChallengeToDto)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(dtoList);
+        } catch (ChallengeNotFoundException e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
         }
-        List<ChallengeDTO> dtoList = challenges.stream().map(challengeDtoConverter::converChallengeToDto)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(dtoList);
+
+        catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "Ocurrió un error interno en el servidor");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+        }
     }
 
     @GetMapping("/filter")
     public ResponseEntity<?> getFilteredChallenges(
             @RequestParam(required = false) List<Long> odsIds,
-             @RequestParam(required = false) BigDecimal min,
-            @RequestParam (required = false) BigDecimal max) {
+            @RequestParam(required = false) BigDecimal min,
+            @RequestParam(required = false) BigDecimal max) {
 
-        List<Ods> odsList = (odsIds != null && !odsIds.isEmpty()) ? odsService.findAllById(odsIds) : null;
-        
+        try {
+            List<Ods> odsList = (odsIds != null && !odsIds.isEmpty()) ? odsService.findAllById(odsIds) : null;
 
-        List<Challenge> challenges = challengeService.getChallengesByFilter(odsList, min, max);
-       
-        if (challenges.isEmpty()) {
-            return ResponseEntity.badRequest().build();
+            List<Challenge> challenges = challengeService.getChallengesByFilter(odsList, min, max);
+
+            if (challenges.isEmpty()) {
+                throw new ChallengeNotFoundException("No se han encontrado challenges con el filtrado");
+            }
+
+            List<ChallengeDTO> dtoList = challenges.stream().map(challengeDtoConverter::converChallengeToDto)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(dtoList);
+        } catch (ChallengeNotFoundException e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "Ocurrió un error interno en el servidor");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
         }
-       
-
-        List<ChallengeDTO> dtoList = challenges.stream().map(challengeDtoConverter::converChallengeToDto)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(dtoList);
 
     }
 
