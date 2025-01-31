e1129fe2a58d3ea50774926b615d161b4da8dbc6
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-01-31 10:23:23 +0100

    Fixed getters and setters from Company

--- src/main/java/com/ecolink/spring/entity/Company.java
+++ src/main/java/com/ecolink/spring/entity/Company.java
@@ -1,13 +1,14 @@
 package com.ecolink.spring.entity;
 
 import jakarta.persistence.Entity;
-
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
-@ToString
+@Getter
+@Setter
 public class Company extends UserBase {
     private String description;
     public Company(String name, String email, Long level, String description) {
@@ -15,14 +16,6 @@ public class Company extends UserBase {
         this.level = (level != null) ? level : 0L;
         this.userType = UserType.COMPANY;
         this.email = email;
-        this.description = description;
-    }
-
-    public String getDescription(){
-        return this.description;
-    }
-
-    public void setDescription(String description){
         this.description = description;
     }
 }
