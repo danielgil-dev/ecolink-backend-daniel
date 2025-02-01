251dc435e544f87c8d55c4440aa194811697f150
Aarón Carrera <aarcarpas@gmail.com>
2025-02-01 16:52:11 +0100

    Added Method To Create UserBase

--- src/main/java/com/ecolink/spring/security/SecurityConfig.java
+++ src/main/java/com/ecolink/spring/security/SecurityConfig.java
@@ -10,30 +10,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
+	@Bean
 	protected BCryptPasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-    @Bean
+	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/**").permitAll()
-					.anyRequest().authenticated()
-			)
-			.csrf(csrf -> csrf
-                .ignoringRequestMatchers("/h2-console/**") // Deshabilitar CSRF para H2 Console
-            )
-            .headers(headers -> headers
-                .frameOptions(frameOptions -> frameOptions.disable()) // Permitir frames para H2 Console
-            )
-			.logout(logout -> logout
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/public")  // Redirige a la página de inicio de sesión después del cierre de sesión
-			.invalidateHttpSession(true)
-            .permitAll()
-        	);
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/**").permitAll()
+						.anyRequest().authenticated())
+				.csrf(csrf -> csrf
+						.ignoringRequestMatchers("/h2-console/**") // Deshabilitar CSRF para H2 Console
+				)
+				.headers(headers -> headers
+						.frameOptions(frameOptions -> frameOptions.disable()) // Permitir frames para H2 Console
+				)
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/public") // Redirige a la página de inicio de sesión después del cierre de
+														// sesión
+						.invalidateHttpSession(true)
+						.permitAll());
 		return http.build();
 	}
 }
--- src/main/java/com/ecolink/spring/service/UserBaseService.java
+++ src/main/java/com/ecolink/spring/service/UserBaseService.java
@@ -1,12 +1,22 @@
 package com.ecolink.spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ecolink.spring.entity.UserBase;
 import com.ecolink.spring.repository.UserBaseRepository;
 
 @Service
 public class UserBaseService {
     @Autowired
-    private UserBaseRepository repository;  
+    private UserBaseRepository repository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserBase newUser(UserBase user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return repository.save(user);
+    }
 }
