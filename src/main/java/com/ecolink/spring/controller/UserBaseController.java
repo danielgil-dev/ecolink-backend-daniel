67bd4a092fd4695843111e88aa6500cfcc6e40f1
dagil23 <da.gil@iesdoctorbalmis.com>
2025-02-03 14:54:34 +0100

    We start whit the logic to the JWT

--- src/main/java/com/ecolink/spring/controller/UserBaseController.java
+++ src/main/java/com/ecolink/spring/controller/UserBaseController.java
@@ -33,6 +33,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -105,4 +109,8 @@ public class UserBaseController {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
+
+ 
+    
+
 }
