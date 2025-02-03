9a4688050f74bc7bfcac660542b2297e9fd05e49
Aarón Carrera <aarcarpas@gmail.com>
2025-02-03 19:24:52 +0100

    Added invoke for incorrect login

--- src/main/java/com/ecolink/spring/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ecolink/spring/security/JwtAuthenticationEntryPoint.java
@@ -1,10 +1,12 @@
 package com.ecolink.spring.security;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import com.ecolink.spring.exception.ErrorDetails;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -15,23 +17,23 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    	private final ObjectMapper mapper;
-	
-	// @Override
-	// public void commence(HttpServletRequest request, HttpServletResponse response,
-	// 		AuthenticationException authException) throws IOException, ServletException {
-		
-	// 	response.setContentType("application/json");
-	// 	response.setStatus(HttpStatus.UNAUTHORIZED.value());
-		
-	// 	//ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, authException.getMessage());
-	// 	//Tranforma la apiError a un String (en formato JSON)
-	// 	String strApiError = mapper.writeValueAsString(apiError);
-		
-	// 	PrintWriter writer = response.getWriter();
-	// 	writer.println(strApiError);
-	// }
+	private final ObjectMapper mapper;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+
+		response.setContentType("application/json");
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+		ErrorDetails errorDetails = new ErrorDetails(HttpStatus.UNAUTHORIZED, authException.getMessage());
+
+		String strApiError = mapper.writeValueAsString(errorDetails);
+
+		PrintWriter writer = response.getWriter();
+		writer.println(strApiError);
+	}
 
 }
