842660f3e1c393cbf6178faa835847bd69431168
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-02-06 11:26:40 +0100

    Added image url into company for challenge

--- src/main/java/com/ecolink/spring/dto/CompanyForChallengeDTO.java
+++ src/main/java/com/ecolink/spring/dto/CompanyForChallengeDTO.java
@@ -1,13 +1,14 @@
 package com.ecolink.spring.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CompanyForChallengeDTO {
-private Long id;
+    private Long id;
     private String name;
     private String email;
+    private String image;
+    private String imageUrl;
 }
