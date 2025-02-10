02726808c3162aa4b86de7a34a97d79ca2ccde0a
dagil23 <da.gil@iesdoctorbalmis.com>
2025-02-10 14:51:58 +0100

    We start to the logic to send to the Front the profile of a user type client

---
+++ src/main/java/com/ecolink/spring/dto/ClientProfileDTO.java
@@ -0,0 +1,24 @@
+package com.ecolink.spring.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientProfileDTO {
+
+    private String name;
+    private Integer level;
+    private List<ClientMissionDTO> completedMissions;
+    private List<PostRelevantDTO> listLikePost;
+
+    //3 post de los que le haya dado like utilizar la propiedad like que tiene el usaurio
+
+
+}
