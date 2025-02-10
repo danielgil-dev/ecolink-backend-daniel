3afad8f72e0e96d03b3294c92e5c8a4338596365
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-02-10 12:16:25 +0100

    Added short description and comments into post dto

--- src/main/java/com/ecolink/spring/dto/PostDTO.java
+++ src/main/java/com/ecolink/spring/dto/PostDTO.java
@@ -2,6 +2,9 @@ package com.ecolink.spring.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import com.ecolink.spring.entity.Comment;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +14,10 @@ public class PostDTO {
     private Long id;
     private String startupName;
     private String title;
+    private String shortDescription;
     private String description;
     private Integer likesCount; 
     List<OdsDTO> odsList;
     private LocalDate postDate;
+    private List<Comment> comments;
 }
