c93e79de970f1af38033916b8a8a75becef461cf
Aarón Carrera <aarcarpas@gmail.com>
2025-02-12 18:15:58 +0100

    Added endpoint to update challenges with user and company validations

--- src/main/java/com/ecolink/spring/controller/ChallengeController.java
+++ src/main/java/com/ecolink/spring/controller/ChallengeController.java
@@ -32,6 +32,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -177,6 +178,69 @@ public class ChallengeController {
                     "Ocurrió un error interno en el servidor");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
         }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateChallenge(@AuthenticationPrincipal UserBase user, @PathVariable Long id, @RequestBody ChallengePostDTO challenge){
+        if (user == null) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.UNAUTHORIZED.value(),
+                    "The user must be logged in");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorDetails);
+        }
+
+        if (!(user instanceof Company)) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.UNAUTHORIZED.value(),
+                    "The user must be a company");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorDetails);
+        }
+
+        Company company = (Company) user;
+        Challenge actuaChallenge = challengeService.findByIdAndCompany(id, company);
+
+        if (actuaChallenge == null) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(),
+                    "The challenge does not exist or does not belong to the company");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+        }
+
+        if (!challenge.getTitle().isEmpty() ) {
+            actuaChallenge.setTitle(challenge.getTitle());
+        }
+
+        if (!challenge.getDescription().isEmpty() ) {
+            actuaChallenge.setDescription(challenge.getDescription());
+        }
+
+        if (!challenge.getShortDescription().isEmpty() ) {
+            actuaChallenge.setShortDescription(challenge.getShortDescription());
+        }
+
+        if (challenge.getBudget() != null ) {
+            actuaChallenge.setBudget(challenge.getBudget());
+        }
+
+        if (challenge.getEndDate() != null ) {
+            actuaChallenge.setEndDate(challenge.getEndDate());
+        }
+
+        if (challenge.getOdsList() != null && !challenge.getOdsList().isEmpty()) {
+            List<Ods> odsList = odsService.findAllById(challenge.getOdsList());
+            actuaChallenge.setOdsList(odsList);
+        }
+
+        if (challenge.getRequirements() != null && !challenge.getRequirements().isEmpty()) {
+            actuaChallenge.setRequirements(challenge.getRequirements());
+        }
+
+        if (challenge.getBenefits() != null && !challenge.getBenefits().isEmpty()) {
+            actuaChallenge.setBenefits(challenge.getBenefits());
+        }
+
+        challengeService.save(actuaChallenge);
+
+        SuccessDetails successDetails = new SuccessDetails(HttpStatus.OK.value(), "Challenge updated successfully");
+
+        return ResponseEntity.status(HttpStatus.OK).body(successDetails);
     }
 
 }
