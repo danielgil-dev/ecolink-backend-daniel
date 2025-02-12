61b22bbd2d12d10c59cb3eef0ca565bb2f0127b1
Aarón Carrera <aarcarpas@gmail.com>
2025-02-12 18:35:39 +0100

    Add endpoint to validate companies and change its status

--- src/main/java/com/ecolink/spring/controller/AdminController.java
+++ src/main/java/com/ecolink/spring/controller/AdminController.java
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecolink.spring.dto.CompanyProfileDTO;
 import com.ecolink.spring.dto.DTOConverter;
 import com.ecolink.spring.dto.StartupPublicProfileDTO;
+import com.ecolink.spring.entity.Company;
 import com.ecolink.spring.entity.Startup;
 import com.ecolink.spring.entity.Status;
+import com.ecolink.spring.exception.CompanyNotFoundException;
 import com.ecolink.spring.exception.ErrorDetails;
 import com.ecolink.spring.exception.StartupNotFoundException;
+import com.ecolink.spring.service.CompanyService;
 import com.ecolink.spring.service.StartupService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RequestMapping("/api/admin")
@@ -26,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AdminController {
 
     private final StartupService startupService;
+    private final CompanyService companyService;
     private final DTOConverter dtoConverter;
 
     @PostMapping("/validate-startup/{id}")
@@ -35,8 +39,9 @@ public class AdminController {
             
             Startup startup = startupService.findById(id);
             if (startup == null) {
-                throw new StartupNotFoundException("No se encontro una startup con el id " + id);
+                throw new StartupNotFoundException("No startup found with the id " + id);
             }
+
             startupService.changeStartupState(startup, state);
             StartupPublicProfileDTO startupDto = dtoConverter.convertStartupProfileToDto(startup);
     
@@ -52,7 +57,32 @@ public class AdminController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
 
         }
+    }
+    @PostMapping("/validate-company/{id}")
+    public ResponseEntity<?> validateCompany(@PathVariable Long id, @RequestParam(required = true) Status state){
 
+        try {
+            
+            Company company = companyService.findById(id);
+            if (company == null) {
+                // devolver comentario en ingles
+                throw new CompanyNotFoundException("No company found with the id" + id);
+            }
+            companyService.changeCompanyState(company, state);
 
+            CompanyProfileDTO companyDto = dtoConverter.convetCompanyToCompanyProfileDTO(company);
+    
+            return ResponseEntity.ok(companyDto);
+        } catch (CompanyNotFoundException  e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    "Ocurrió un error interno en el servidor");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
+
+        }
     }
 }
