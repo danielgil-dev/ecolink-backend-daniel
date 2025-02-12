62aef211ce8af608eecc71d3599161aafb3b874a
Aarón Carrera <aarcarpas@gmail.com>
2025-02-12 18:35:39 +0100

    Add endpoint to validate companies and change its status

--- src/main/java/com/ecolink/spring/service/CompanyService.java
+++ src/main/java/com/ecolink/spring/service/CompanyService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecolink.spring.entity.Company;
+import com.ecolink.spring.entity.Status;
 import com.ecolink.spring.repository.CompanyRepository;
 
 @Service
@@ -12,22 +13,40 @@ public class CompanyService {
 
     @Autowired
     private CompanyRepository repository;
-    
-    public boolean existsByName(String name){
+
+    public boolean existsByName(String name) {
         return repository.existsByName(name);
     }
-    public void save (Company company){
+
+    public void save(Company company) {
         repository.save(company);
     }
 
-    public Company findByName(String name){
+    public Company findByName(String name) {
         return repository.findByName(name);
     }
 
-    public List<Company> getAllCompanies(){
+    public List<Company> getAllCompanies() {
         return repository.findAll();
     }
-    public Company getCompanyById(Long id){
+
+    public Company getCompanyById(Long id) {
         return repository.findById(id).orElse(null);
+    }
+
+    public Company findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void changeCompanyState(Company company, Status state) {
+
+        if (company.getStatus() == Status.PENDING && state == Status.ACCEPTED) {
+            company.setStatus(state);
+        } else if (company.getStatus() == Status.PENDING && state == Status.REJECTED) {
+            company.setStatus(state);
+        } else {
+            company.setStatus(Status.PENDING);
+        }
+
     }
 }
