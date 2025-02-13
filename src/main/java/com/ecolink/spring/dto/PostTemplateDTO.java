1cc06d94d03097784e79eccc8a597f24bd3a896b
dagil23 <da.gil@iesdoctorbalmis.com>
2025-02-13 13:53:35 +0100

    Starting whit the logic to crear a post

---
+++ src/main/java/com/ecolink/spring/dto/PostTemplateDTO.java
@@ -0,0 +1,21 @@
+package com.ecolink.spring.dto;
+
+import org.springframework.cglib.core.Local;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class PostTemplateDTO {
+
+    private String title;
+    private String shortDescription;
+    private String description;
+    private String imageUrl;
+}
