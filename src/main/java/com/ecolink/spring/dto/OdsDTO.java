78b7c2aafbc9e27c2d56bfa986fbdbc2b135aab4
Aarón Carrera <aarcarpas@gmail.com>
2025-02-16 16:24:19 +0100

    Add image field to OdsDTO and OdsWithoutIdDTO

--- src/main/java/com/ecolink/spring/dto/OdsDTO.java
+++ src/main/java/com/ecolink/spring/dto/OdsDTO.java
@@ -8,4 +8,5 @@ import lombok.Setter;
 public class OdsDTO {
     private Long id;
     private String name;
+    private String image;
 }
--- src/main/java/com/ecolink/spring/dto/OdsWithoutIdDTO.java
+++ src/main/java/com/ecolink/spring/dto/OdsWithoutIdDTO.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Setter
 public class OdsWithoutIdDTO {
     private String name;
+    private String image;
 }
