4c65c4a1f19ce69fe0428ec38d94b34365366341
AarCar <aaron.carrera@iesdoctorbalmis.com>
2025-02-18 13:53:37 +0100

    Add Lombok annotations for getter and setter in Startup entity

--- src/main/java/com/ecolink/spring/entity/Startup.java
+++ src/main/java/com/ecolink/spring/entity/Startup.java
@@ -17,13 +17,17 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @ToString
 public class Startup extends UserBase {
 
